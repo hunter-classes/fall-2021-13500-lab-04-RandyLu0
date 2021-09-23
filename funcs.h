52f80a957bb81b24c52817fc3ffdba60fd82f27c
RandyLu0 <lurandy8@gmail.com>
2021-09-23 18:46:35 -0400

    added funcs.h

---
+++ funcs.h
@@ -0,0 +1,22 @@
+/*
+ * Name: Randy Lu
+ * Instructor: Mr. Zamansky
+ * Assignment: Lab 4
+ * Purpose: Prototypes the functions
+ * defined in funcs.cpp
+ */
+#pragma once
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include <string>
+
+std::string box(int w, int h);
+std::string board(int w, int h);
+std::string cross(int s);
+std::string lower(int l);
+std::string upper(int l);
+std::string trap(int w, int h);
+std::string board3(int w, int h);
+
+#endif
